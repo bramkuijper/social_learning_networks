0699228127f0f71b74b535704971be6f7111fc1a
Bram Kuijper <bramkuijper@gmail.com>
2023-05-19 14:01:21 +0100

    adding command line args to the simulation so that we can vary the file name

--- src/ibm/network_main.cpp
+++ src/ibm/network_main.cpp
@@ -6,6 +6,11 @@ int main(int argc, char **argv)
 {
     Parameters params;
 
+    std::string temp_file_name_str(argv[1]);
+
+    params.base_name = temp_file_name_str + "_stats";
+    params.base_name_matrix_file = temp_file_name_str + "_data";
+
     Simulation sim(params);
 
     sim.run();
---
+++ src/ibm/script_to_run_my_sims.sh
@@ -0,0 +1,8 @@
+./social_learning_network.exe file_0
+./social_learning_network.exe file_1
+./social_learning_network.exe file_2
+./social_learning_network.exe file_3
+./social_learning_network.exe file_4
+./social_learning_network.exe file_5
+./social_learning_network.exe file_6
+./social_learning_network.exe file_7
